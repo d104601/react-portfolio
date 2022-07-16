8dba373c592096ed37be32e1ca84c2f1ec8db819
Taeyong Lee <d104601@hotmail.com>
2022-07-16 01:27:51 -0400

    heroku upload

--- src/components/main-projects.js
+++ src/components/main-projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 function projects() {
     return (
         <div class="container text-center justify-content-center">
-            <h1 class="my-3">My awesome projects will be added soon</h1>
+            <h1 class="my-3">Projects</h1>
         </div>
     );
 }
